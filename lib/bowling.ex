bb0771c5b1976fd169ebe2c07a829030aea9140b
Sambe <a01739259@tec.mx>
2026-02-19 22:56:29 -0600

    Section 5: Implemented Strike logic contemplating the 10th round 'Perfect game'#

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -2,7 +2,14 @@ defmodule Bowling do
 
   #Redisigned/simpler code version
 
+  #Stopping the recurssion
   def score([]), do: 0
+
+  #10th round logic
+  def score([[f, s, t] | []]) do
+    #It will sum the last 3 shots to the 10th round
+    (f||0) + (s||0) + (t||0) #guarantees to not crash when detecting a nil
+  end
 
   def score([head|tail]) do
     #first number equals the first element of the list "head"
@@ -29,18 +36,27 @@ defmodule Bowling do
     end
   end
 
+
+
+
   #This function will sum the following two shots to the shot that triggered the Strike.
   defp strike_bonus(tail) do
     next_round = Enum.at(tail, 0)
     verynext_round = Enum.at(tail, 1)
 
-    #Checks if the next shot was also a strike, so that it guarantees grabbing the real next shot
+    #Checks if the next shot was also a strike, so that it guarantees grabbing the real next shot (Implemented 10th round logic)
     if Enum.at(next_round, 0) == 10 do
-      10 + Enum.at(verynext_round, 0)
+      #It checks weather the next round is the 10th or not, by comparing the number of elements in the list
+      if length(next_round) == 3 do
+        10 + Enum.at(next_round, 1)
+      #Rounds <10
+      else
+        10 + Enum.at(verynext_round || [0], 0) #guarantees to not crash when detecting a nil
+      end
 
     #Discards the next shot being a strike and grabs the shot of the next round
     else
-      Enum.at(next_round, 0) + Enum.at(next_round, 1)
+      (Enum.at(next_round, 0) || 0) + (Enum.at(next_round, 1)|| 0) #guarantees to not crash when detecting a nil
     end
   end
 
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -24,4 +24,10 @@ defmodule BowlingTest do
     game = [[10,nil],[3,4]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
     assert Bowling.score(game) == 24
   end
+
+  #test given by professor
+  test "perfect game" do
+    game = List.duplicate([10,nil], 9) ++ [[10,10,10]]
+    assert Bowling.score(game) == 300
+  end
 end
