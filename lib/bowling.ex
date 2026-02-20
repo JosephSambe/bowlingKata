236ff91b4d372784895c4252b764e9063a9a0a31
Sambe <a01739259@tec.mx>
2026-02-19 21:27:41 -0600

    Section 3: Implemented Spare logic with helper functions

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -7,7 +7,31 @@ defmodule Bowling do
     first = Enum.at(head, 0)
     #second number equals the second element of the list "head"
     second = Enum.at(head, 1)
-    #adds first + second + a recurssion to its own by sending the tail and beginning the process again until reaching ([])
+
+    #THIS WILL ONLY WORK WITH THIS SPECIFIC TEST, IT WONT WORK FOR THE STRIKE!!
+    #if the total punctiation == 10, it adds first + second + another fucntion triggered by the attom :spare, sending the tail and
+    #following its explained logic
+    if first + second == 10 do
+      first + second + score(tail, :spare)
+    #if not, it adds first + second + a recurssion to its own by sending the tail and beginning the process again until reaching ([])
+    else
     first + second + score(tail)
+    end
+  end
+
+  def score([head|tail], :spare) do
+    #first number equals the first element of the list "head"
+    first = Enum.at(head, 0)
+    #second number equals the second element of the list "head"
+    second = Enum.at(head, 1)
+
+    #THIS WILL ONLY WORK WITH THIS SPECIFIC TEST, IT WONT WORK FOR THE STRIKE!!
+    #if the total punctiation == 10, it adds first + second + a recurssion to its own by sending the tail and the attom :spare
+    if first + second == 10 do
+      (first * 2) + second + score(tail, :spare)
+    else
+    #if not, it adds first + second + a call to the normal function sending its tail
+    (first * 2) + second + score(tail)
+    end
   end
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -1,15 +1,22 @@
 defmodule BowlingTest do
   use ExUnit.Case
 
-  #test guven by professor
+  #test given by professor
   test "gutter game" do
     game = List.duplicate([0, 0], 9) ++ [[0, 0, nil]]
     assert Bowling.score(game) == 0
   end
 
-  #test guven by professor
+  #test given by professor
   test "'all ones' game" do
     game = List.duplicate([1,1], 9) ++ [[1, 1, nil]]
     assert Bowling.score(game) == 20
   end
+
+  #test given by professor
+  test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 16
+  end
+
 end
